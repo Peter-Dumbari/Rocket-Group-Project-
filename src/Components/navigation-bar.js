56053c26a15009debb8b2c87261416ac33955a91
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-06-28 11:36:46 +0100

    add images and NavLink on our navbar

--- src/Components/navigation-bar.js
+++ src/Components/navigation-bar.js
@@ -1,16 +1,29 @@
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import logo from '../img/planet.png';
 
 function NavigationBar() {
   return (
     <div className="nav">
       <div className="logo">
-        <img src="../img/planet.png" alt="" />
+        <img src={logo} alt="logo" />
         <p className="campany-name">Space Travels Hub</p>
       </div>
       <ul>
-        <li><Link to="/" className="Link">Rocket</Link></li>
-        <li><Link to="/mission" className="Link">Mission</Link></li>
-        <li className=""><Link to="/myprofile" className="Link last">My Profile</Link></li>
+        <li>
+          <NavLink to="/" className="Link" activeClassName="active" exact>
+            Rocket
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/mission" className="Link" activeClassName="active">
+            Mission
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/myprofile" className="Link" activeClassName="active">
+            My Profile
+          </NavLink>
+        </li>
       </ul>
     </div>
   );
