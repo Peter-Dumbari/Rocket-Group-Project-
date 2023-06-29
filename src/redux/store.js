1f62e22e4d5dc201060f7980133c03948b52dd53
TAMS-TECHNOLOGY <dummytwiz@gmail.com>
2023-06-30 00:04:06 +0100

    updates

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 
-import rocketSlice from "./Rocket/rocketSlice";
+import rocketSlice from './Rocket/rocketSlice';
 
 const store = configureStore({
   reducer: {
