c02ce5f0352e5e19bc78cd52a786c143ca3fe820
Gergito Kusse <gergito2@gmail.com>
2023-06-30 15:18:00 +0300

    Fix resevred and canceling rocket booking

--- src/Components/Rockect.js
+++ src/Components/Rockect.js
@@ -1,14 +1,8 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import RocketItem from './RocketItem';
-import { getData } from '../redux/Rocket/rocketSlice';
 
 const Rockets = () => {
   const { rockets, isLoading, error } = useSelector((store) => store.rockets);
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getData());
-  }, [dispatch]);
 
   if (error) {
     return <div>{error.message}</div>;
